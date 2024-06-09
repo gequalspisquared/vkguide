eab32ed3d5240de5180d8bbac7e103aed007415a
Nicholas Crane <nickcrusher30@gmail.com>
2024-06-09 13:59:51 -0600

    Implement source code of chapter 0

---
+++ src/inc/vk_engine.h
@@ -0,0 +1,30 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vk_types.h>
+
+class VulkanEngine {
+public:
+	static VulkanEngine& Get();
+
+	//initializes everything in the engine
+	void init();
+
+	//shuts down the engine
+	void cleanup();
+
+	//draw loop
+	void draw();
+
+	//run main loop
+	void run();
+
+private:
+	bool m_is_initialized { false };
+	int m_frame_number { 0 };
+	bool m_stop_rendering { false };
+	VkExtent2D m_window_extent { 1700 , 900 };
+	struct SDL_Window* m_window { nullptr };
+};
---
+++ src/vk_engine.cpp
@@ -0,0 +1,87 @@
+﻿//> includes
+#include "vk_engine.h"
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+
+#include <vk_initializers.h>
+#include <vk_types.h>
+
+#include <chrono>
+#include <thread>
+
+VulkanEngine* loaded_engine = nullptr;
+
+VulkanEngine& VulkanEngine::Get() { return *loaded_engine; }
+void VulkanEngine::init()
+{
+    // only one engine initialization is allowed with the application.
+    assert(loaded_engine == nullptr);
+    loaded_engine = this;
+
+    // We initialize SDL and create a window with it.
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+
+    m_window = SDL_CreateWindow(
+        "Vulkan Engine",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        m_window_extent.width,
+        m_window_extent.height,
+        window_flags);
+
+    // everything went fine
+    m_is_initialized = true;
+}
+
+void VulkanEngine::cleanup()
+{
+    if (m_is_initialized) {
+
+        SDL_DestroyWindow(m_window);
+    }
+
+    // clear engine pointer
+    loaded_engine = nullptr;
+}
+
+void VulkanEngine::draw()
+{
+    // nothing yet
+}
+
+void VulkanEngine::run()
+{
+    SDL_Event e;
+    bool quit = false;
+
+    // main loop
+    while (!quit) {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            // close the window when user alt-f4s or clicks the X button
+            if (e.type == SDL_QUIT)
+                quit = true;
+
+            if (e.type == SDL_WINDOWEVENT) {
+                if (e.window.event == SDL_WINDOWEVENT_MINIMIZED) {
+                    m_stop_rendering = true;
+                }
+                if (e.window.event == SDL_WINDOWEVENT_RESTORED) {
+                    m_stop_rendering = false;
+                }
+            }
+        }
+
+        // do not draw if we are minimized
+        if (m_stop_rendering) {
+            // throttle the speed to avoid the endless spinning
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            continue;
+        }
+
+        draw();
+    }
+}
