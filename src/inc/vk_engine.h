1b0545bf7aae925cf65ef2e8178cb8f73b8f5ba3
Nicholas Crane <nickcrusher30@gmail.com>
2024-06-09 21:12:06 -0600

    Initialize instance, device, debug messenger, physical device, and swapchain

--- src/inc/vk_engine.h
+++ src/inc/vk_engine.h
@@ -27,4 +27,24 @@ private:
 	bool m_stop_rendering { false };
 	VkExtent2D m_window_extent { 1700 , 900 };
 	struct SDL_Window* m_window { nullptr };
+
+	VkInstance m_instance;
+	VkDebugUtilsMessengerEXT m_debug_messenger;
+	VkPhysicalDevice m_chosen_gpu;
+	VkDevice m_device;
+	VkSurfaceKHR m_surface;
+
+	VkSwapchainKHR m_swapchain;
+	VkFormat m_swapchain_image_format;
+	std::vector<VkImage> m_swapchain_images;
+	std::vector<VkImageView> m_swapchain_image_views;
+	VkExtent2D m_swapchain_extent;
+
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -3,14 +3,17 @@
 
 #include <SDL.h>
 #include <SDL_vulkan.h>
+#include <VkBootstrap.h>
 
 #include <vk_initializers.h>
 #include <vk_types.h>
+
 
 #include <chrono>
 #include <thread>
 
 VulkanEngine* loaded_engine = nullptr;
+constexpr bool use_validation_layers = true;
 
 VulkanEngine& VulkanEngine::Get() { return *loaded_engine; }
 void VulkanEngine::init()
@@ -32,6 +35,11 @@ void VulkanEngine::init()
         m_window_extent.height,
         window_flags);
 
+    init_vulkan();
+    init_swapchain();
+    init_commands();
+    init_sync_structures();
+
     // everything went fine
     m_is_initialized = true;
 }
@@ -39,6 +47,13 @@ void VulkanEngine::init()
 void VulkanEngine::cleanup()
 {
     if (m_is_initialized) {
+        destroy_swapchain();
+
+        vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
+        vkDestroyDevice(m_device, nullptr);
+
+        vkb::destroy_debug_utils_messenger(m_instance, m_debug_messenger);
+        vkDestroyInstance(m_instance, nullptr);
 
         SDL_DestroyWindow(m_window);
     }
@@ -73,6 +88,13 @@ void VulkanEngine::run()
                     m_stop_rendering = false;
                 }
             }
+
+            if (e.type == SDL_KEYDOWN) {
+                if (e.key.keysym.sym == SDLK_ESCAPE) {
+                    m_stop_rendering = false;
+                    quit = true;
+                }
+            }
         }
 
         // do not draw if we are minimized
@@ -83,5 +105,92 @@ void VulkanEngine::run()
         }
 
         draw();
+    }
+}
+
+void VulkanEngine::init_vulkan() {
+    vkb::InstanceBuilder builder;
+
+    auto inst_ret = builder
+        .set_app_name("Example Vulkan Application")
+        .request_validation_layers(use_validation_layers)
+        .use_default_debug_messenger()
+        .require_api_version(1, 3, 0)
+        .build();
+    
+    vkb::Instance vkb_inst = inst_ret.value();
+
+    m_instance = vkb_inst.instance;
+    m_debug_messenger = vkb_inst.debug_messenger;
+
+    SDL_Vulkan_CreateSurface(m_window, m_instance, &m_surface);
+
+    VkPhysicalDeviceVulkan13Features features {
+        .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES
+    };
+    features.dynamicRendering = true;
+    features.synchronization2 = true;
+
+    VkPhysicalDeviceVulkan12Features features12 {
+        .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES
+    };
+    features12.bufferDeviceAddress = true;
+    features12.descriptorIndexing = true;
+
+    vkb::PhysicalDeviceSelector selector { vkb_inst };
+    vkb::PhysicalDevice physical_device = selector
+        .set_minimum_version(1, 3)
+        .set_required_features_13(features)
+        .set_required_features_12(features12)
+        .set_surface(m_surface)
+        .select()
+        .value();
+    
+    vkb::DeviceBuilder device_builder { physical_device };
+    vkb::Device vkb_device = device_builder.build().value();
+
+    m_device = vkb_device.device;
+    m_chosen_gpu = physical_device.physical_device;
+}
+
+void VulkanEngine::init_swapchain() {
+    create_swapchain(m_window_extent.width, m_window_extent.height);
+}
+
+void VulkanEngine::init_commands() {
+
+}
+
+void VulkanEngine::init_sync_structures() {
+
+}
+
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height) {
+    vkb::SwapchainBuilder swapchain_builder { m_chosen_gpu, m_device, m_surface };
+    m_swapchain_image_format = VK_FORMAT_B8G8R8A8_UNORM;
+
+    vkb::Swapchain vkb_swapchain = swapchain_builder
+        .set_desired_format(VkSurfaceFormatKHR { 
+            .format = m_swapchain_image_format,
+            .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR
+        })
+        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+        .set_desired_extent(width, height)
+        .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+        .build()
+        .value();
+    
+    m_swapchain_extent = vkb_swapchain.extent;
+    m_swapchain = vkb_swapchain.swapchain;
+    m_swapchain_images = vkb_swapchain.get_images().value();
+    m_swapchain_image_views = vkb_swapchain.get_image_views().value();
+}
+
+void VulkanEngine::destroy_swapchain() {
+    vkDestroySwapchainKHR(m_device, m_swapchain, nullptr);
+
+    // destroy swapchain resources
+    for (int i = 0; i < m_swapchain_image_views.size(); i++) {
+        vkDestroyImageView(m_device, m_swapchain_image_views[i], nullptr);
     }
 }
